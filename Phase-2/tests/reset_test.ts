5c325d575d163668f3db4ef15b5176ffc46009e9
Elijah Jorgensen <jorgenel@purdue.edu>
2024-10-23 21:39:02 -0400

    fmt code

--- Phase-2/tests/reset_test.ts
+++ Phase-2/tests/reset_test.ts
@@ -6,22 +6,22 @@ import { cleanup, setup, testLogger } from "./testSuite.ts";
 const TESTNAME = "resetDatabase";
 
 Deno.test(TESTNAME, async () => {
-    // pre test setup
-    testLogger.info(`TEST: ${TESTNAME}`);
-    let db: DB | null = null;
+	// pre test setup
+	testLogger.info(`TEST: ${TESTNAME}`);
+	let db: DB | null = null;
 
-    db = await setup(TESTNAME);
-    await resetDatabase(db);
+	db = await setup(TESTNAME);
+	await resetDatabase(db);
 
-    const packages: Row[] = await db.query(`SELECT * FROM packages`);
-    const users: Row[] = await db.query(`SELECT * FROM users`);
+	const packages: Row[] = await db.query(`SELECT * FROM packages`);
+	const users: Row[] = await db.query(`SELECT * FROM users`);
 
-    assertEquals(packages.length, 0, "Packages table should be empty");
-    assertEquals(users.length, 2, "Users table should have 2 entries");
+	assertEquals(packages.length, 0, "Packages table should be empty");
+	assertEquals(users.length, 2, "Users table should have 2 entries");
 
-    testLogger.debug("resetDatabase test passed");
+	testLogger.debug("resetDatabase test passed");
 
-    if (db) {
-        await cleanup(db, TESTNAME);
-    }
+	if (db) {
+		await cleanup(db, TESTNAME);
+	}
 });
--- Phase-2/tests/testSuite.ts
+++ Phase-2/tests/testSuite.ts
@@ -3,31 +3,31 @@ import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import { populateDatabase } from "../utils/populateDatabase.ts";
 
 export async function setup(dbname: string) {
-    //setup test database in memory
-    const db = new DB(":memory:");
-    await populateDatabase(db);
-    return db;
+	//setup test database in memory
+	const db = new DB(":memory:");
+	await populateDatabase(db);
+	return db;
 }
 
 export const testLogger: Logger = createLogger({
-    level: "debug",
-    format: format.combine(
-        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`),
-    ),
-    transports: [
-        new transports.File({
-            filename: "logs/tests.log",
-            options: { flags: "a" },
-        }),
-    ],
+	level: "debug",
+	format: format.combine(
+		format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+		format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`),
+	),
+	transports: [
+		new transports.File({
+			filename: "logs/tests.log",
+			options: { flags: "a" },
+		}),
+	],
 });
 
 export async function cleanup(db: DB, filename: string) {
-    // delete the data.db file
-    // if the database is open, close it
-    if (!db.isClosed) {
-        await db.close(true);
-    }
-    testLogger.debug("End Test");
+	// delete the data.db file
+	// if the database is open, close it
+	if (!db.isClosed) {
+		await db.close(true);
+	}
+	testLogger.debug("End Test");
 }
