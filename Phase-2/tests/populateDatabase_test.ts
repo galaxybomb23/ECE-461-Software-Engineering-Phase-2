5803f1d4361fe4b230bfbd58f2add4e31f22ad87
Elijah Jorgensen <jorgenel@purdue.edu>
2024-10-23 21:28:23 -0400

    add fmt to CI

--- Phase-2/tests/populateDatabase_test.ts
+++ Phase-2/tests/populateDatabase_test.ts
@@ -6,25 +6,25 @@ import { cleanup, setup, testLogger } from "./testSuite.ts";
 const TESTNAME = "populateDatabase";
 
 Deno.test(TESTNAME, async () => {
-    // setup
-    testLogger.info(`TEST: ${TESTNAME}`);
-    let db: DB | null = null;
+	// setup
+	testLogger.info(`TEST: ${TESTNAME}`);
+	let db: DB | null = null;
 
-    try {
-        db = await setup(TESTNAME);
-        await populateDatabase(db);
-        const packages: Row[] = await db.query(`SELECT * FROM packages`);
-        assertEquals(packages.length, 2, "Packages table should have 2 entries");
-        const users: Row[] = await db.query(`SELECT * FROM users`);
-        assertEquals(users.length, 2, "Users table should have 2 entries");
+	try {
+		db = await setup(TESTNAME);
+		await populateDatabase(db);
+		const packages: Row[] = await db.query(`SELECT * FROM packages`);
+		assertEquals(packages.length, 2, "Packages table should have 2 entries");
+		const users: Row[] = await db.query(`SELECT * FROM users`);
+		assertEquals(users.length, 2, "Users table should have 2 entries");
 
-        testLogger.debug("populateDatabase test passed");
-    } catch (error) {
-        testLogger.error(`Test failed: ${error}`);
-    } finally {
-        if (db) {
-            // cleanup
-            await cleanup(db, TESTNAME);
-        }
-    }
+		testLogger.debug("populateDatabase test passed");
+	} catch (error) {
+		testLogger.error(`Test failed: ${error}`);
+	} finally {
+		if (db) {
+			// cleanup
+			await cleanup(db, TESTNAME);
+		}
+	}
 });
