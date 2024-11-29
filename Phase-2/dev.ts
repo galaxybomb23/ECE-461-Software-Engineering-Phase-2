3044cb4594331e7e618a18808a783d3095a27cfd
Elijah Jorgensen <jorgenel@purdue.edu>
2024-11-29 16:14:05 -0600

    rm unused lines

--- Phase-2/dev.ts
+++ Phase-2/dev.ts
@@ -2,9 +2,8 @@
 
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
-// import { DB } from "https://deno.land/x/sqlite@v3.9.1/mod.ts"; // SQLite3 import
 import "$std/dotenv/load.ts";
-// import { populateDatabase } from "~/utils/populateDatabase.ts";
+import { logger } from "~/src/logFile.ts";
 
 // await populateDatabase();
 logger.info("Starting dev server...");
