e5d70b41242f7f1e512e1f47807f28a0e9a9c4dd
Elijah Jorgensen <jorgenel@purdue.edu>
2024-12-02 09:54:45 -0500

    update all-server logfile path

--- Phase-2/src/logFile.ts
+++ Phase-2/src/logFile.ts
@@ -35,6 +35,6 @@ export const logger: Logger = createLogger({
 	),
 	transports: [
 		new transports.File({ filename: logFilePath, options: { flags: "a" } }),
-		new transports.File({ filename: "server-all.log", level: "silly", options: { flags: "a" } }),
+		new transports.File({ filename: "logs/server-all.log", level: "silly", options: { flags: "a" } }),
 	],
 });
